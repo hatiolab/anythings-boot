153362af780e751ee23e1fd3eeed46c409ad0dde
wryang <yang@yang-mac.local>
2019-07-18 19:28:34 +0900

    개발환경 구성
    elastic / rabbitmq 구성
    프로퍼티 수정.

--- src/main/java/xyz/anythings/boot/AnythingsBootApplication.java
+++ src/main/java/xyz/anythings/boot/AnythingsBootApplication.java
@@ -8,6 +8,10 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import xyz.anythings.sys.event.EventPublisher;
+import xyz.anythings.sys.event.model.AppsEvent;
+import xyz.elidom.util.BeanUtil;
+
 @EnableAsync
 @EnableScheduling
 @SpringBootApplication
@@ -15,7 +19,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ComponentScan(basePackages = { "xyz.anythings.*", "xyz.elidom.*" })
 @ImportResource({ "classpath:/WEB-INF/application-context.xml", "classpath:/WEB-INF/dataSource-context.xml" })
 public class AnythingsBootApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AnythingsBootApplication.class, args);
+		
+		AppsEvent event = new AppsEvent();
+		event.setAppsStatus("started");
+		
+		BeanUtil.get(EventPublisher.class).publishEvent(event);
 	}
 }
