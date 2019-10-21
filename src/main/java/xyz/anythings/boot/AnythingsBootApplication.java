426fa4365c684e0873df4f2286eead2ff6e5a927
wryang <yang@yang-mac.local>
2019-10-21 19:23:16 +0900

    async annotation 추가
    1

--- src/main/java/xyz/anythings/boot/AnythingsBootApplication.java
+++ src/main/java/xyz/anythings/boot/AnythingsBootApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableAsync
+@EnableAsync(proxyTargetClass = true)
 @EnableScheduling
 @SpringBootApplication
 @EnableConfigurationProperties
