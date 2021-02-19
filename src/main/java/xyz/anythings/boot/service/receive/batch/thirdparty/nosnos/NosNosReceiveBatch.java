99dade9f7d8d1d40d448041310510e4d811aee29
hatiolab <shortstop@hatiolab.com>
2021-02-19 16:19:52 +0900

    NosNos 주문 수신 완료

--- src/main/java/xyz/anythings/boot/service/receive/batch/thirdparty/nosnos/NosNosReceiveBatch.java
+++ src/main/java/xyz/anythings/boot/service/receive/batch/thirdparty/nosnos/NosNosReceiveBatch.java
@@ -21,18 +21,23 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import operato.logis.wcs.entity.Wave;
 import xyz.anythings.base.LogisConstants;
 import xyz.anythings.base.entity.JobBatch;
 import xyz.anythings.base.event.main.BatchReceiveEvent;
 import xyz.anythings.sys.service.ICustomService;
+import xyz.elidom.dbist.dml.Page;
 import xyz.elidom.exception.server.ElidomRuntimeException;
 import xyz.elidom.orm.IQueryManager;
 import xyz.elidom.sys.SysConstants;
 import xyz.elidom.sys.util.SettingUtil;
 import xyz.elidom.sys.util.ThrowUtil;
+import xyz.elidom.util.BeanUtil;
 import xyz.elidom.util.DateUtil;
 import xyz.elidom.util.ValueUtil;
 
@@ -82,26 +87,14 @@ public class NosNosReceiveBatch {
 		// 1. 배치 서머리 (회차 정보) 수신
 		Map<String, Object> summary = this.receiveBatchSummary(event.getJobDate());
 		
-		// 2. 서머리 수신 결과 체크
-		if(summary == null || !ValueUtil.toBoolean(summary.get("status"))) {
-			throw ThrowUtil.newValidationErrorWithNoLog("수신할 정보가 없습니다.");
-		}
+		// 2. 수신할 회차 정보 추출
+		Map<String, Object> response = (Map)this.parseApiResult(summary, null);
+		List<Map> batchSeqList = (List<Map>)this.parseDateList(response, "shipping_info", "수신할 회차 정보가 없습니다.");
 		
-		// 3. 수신할 회차 정보 추출
-		Map<String, Object> response = (Map<String, Object>)summary.get("response");
-		List<Map> batchSeqList = (List<Map>)response.get("shipping_info");
-		
-		if(ValueUtil.isEmpty(batchSeqList)) {
-			throw ThrowUtil.newValidationErrorWithNoLog("수신할 회차 정보가 없습니다.");
-		}
-		
+		// 3. 작업 차수 파싱
 		List<String> jobSeqList = new ArrayList<String>();
 		for(Map batchSeq : batchSeqList) {
 			jobSeqList.add(ValueUtil.toString(batchSeq.get("seq")));
-		}
-		
-		if(ValueUtil.isEmpty(jobSeqList)) {
-			throw ThrowUtil.newValidationErrorWithNoLog("수신할 회차 정보가 없습니다.");
 		}
 		
 		// 4. 이벤트에 수신할 회차 정보 결과 설정
@@ -130,8 +123,8 @@ public class NosNosReceiveBatch {
 		List<String> jobSeqList = (List<String>)event.getResult();
 		
 		// 2. 회차 정보 중에 이마 수신 받은 차수를 배치에서 찾아서 있으면 제거
-		Map<String, Object> params = ValueUtil.newMap("domainId,jobDate,jobSeqList,jobType,areaCd,stageCd,comCd", event.getDomainId(), event.getJobDate(), jobSeqList, event.getJobType(), event.getAreaCd(), event.getStageCd(), event.getComCd());
-		String sql = "select job_seq from job_batches where domain_id = :domainId and job_date = :jobDate and job_seq in (:jobSeqList) #if($jobType) and job_type = :jobType #end #if($areaCd) and area_cd = :areaCd #end #if($stageCd) and stage_cd = :stageCd #end #if($comCd) and com_cd = :comCd #end";
+		Map<String, Object> params = ValueUtil.newMap("domainId,jobDate,jobSeqList,comCd", event.getDomainId(), event.getJobDate(), jobSeqList, event.getComCd());
+		String sql = "select job_seq from waves where domain_id = :domainId and job_date = :jobDate and job_seq in (:jobSeqList) #if($comCd) and com_cd = :comCd #end";
 		List<String> receivedJobSeqList = this.queryManager.selectListBySql(sql, params, String.class, 0, 0);
 		
 		// 3. 수신할 차수 필터링
@@ -144,25 +137,29 @@ public class NosNosReceiveBatch {
 		}
 		
 		// 4. 새로운 회차에 대해서 주문 수신 시작
-		int totalOrders = 0;
+		List<Wave> receivedWaves = new ArrayList<Wave>();
+		NosNosReceiveBatch self = BeanUtil.get(NosNosReceiveBatch.class);
+		
 		for(String jobSeq : jobSeqList) {
-			int batchSeq = ValueUtil.toInteger(jobSeq);
-			Map<String, Object> response = this.receiveOrdersByJobSeq(event.getJobDate(), batchSeq, 1);
-			JobBatch batch = this.configureBatchByOrders(event, batchSeq, response);
+			int waveSeq = ValueUtil.toInteger(jobSeq);
+			Map<String, Object> response = this.receiveOrdersByJobSeq(event.getJobDate(), waveSeq, 1);
 			
-			if(batch != null) {
-				totalOrders += batch.getBatchOrderQty();
+			// 별도 트랜잭션으로 처리 필요 ...
+			Wave wave = self.configureWaveByOrders(event, waveSeq, response);
+			
+			if(wave != null) {
+				receivedWaves.add(wave);
 			}
 		}
 		
-		if(totalOrders == 0) {
+		if(receivedWaves.isEmpty()) {
 			throw ThrowUtil.newValidationErrorWithNoLog("수신할 주문 정보가 없습니다.");
 		}
-		
-		// 5. 커스텀 서비스 호출 
-		Map<String, Object> parameters = ValueUtil.newMap("event", event);
-		this.customService.doCustomService(event.getDomainId(), DIY_HANDLE_BATCHES_BY_NOSNOS, parameters);
 	}
+	
+	/*************************************************************************************************/
+	/*											수 신 로 직  											 */
+	/*************************************************************************************************/
 
 	/**
 	 * 수신 받을 주문 서머리 (회차 정보)를 조회
@@ -225,98 +222,168 @@ public class NosNosReceiveBatch {
 	}
 	
 	/**
-	 * 주문으로 배치 구성
+	 * 주문으로 Wave 구성
 	 * 
 	 * @param event
-	 * @param batchSeq
+	 * @param waveSeq
 	 * @param result
 	 * @return
 	 */
 	@SuppressWarnings("unchecked")
-	private JobBatch configureBatchByOrders(BatchReceiveEvent event, int batchSeq, Map<String, Object> result) {
-		// 1. 주문 체크
-		if(result == null || !ValueUtil.toBoolean(result.get("status"))) {
-			return null;
-		}
+	@Transactional(propagation=Propagation.REQUIRES_NEW)
+	public Wave configureWaveByOrders(BatchReceiveEvent event, int waveSeq, Map<String, Object> result) {
+		// 1. 조회 결과 파싱
+		Map<String, Object> response = (Map<String, Object>)this.parseApiResult(result, null);
 		
-		// 2. 총 주문 건수
-		Map<String, Object> response = (Map<String, Object>)result.get("response");
+		// 2. 총 주문 수 파싱
 		int totalOrders = ValueUtil.toInteger(response.get("release_cnt"));
 		if(totalOrders == 0) {
 			return null;
 		}
 		
-		// 3. 작업 배치 생성
-		JobBatch batch = this.createNewBatch(event, batchSeq);
+		// 3. Wave 생성
+		Wave wave = this.createNewWave(event, waveSeq);
 		
 		// 4. 수신 데이터로 주문 생성
-		this.createOrdersByBatch(batch, result);
+		this.createOrdersByWave(event, wave, result);
 		
-		// 5. 배치 생성
-		batch.setParentOrderQty(totalOrders);
-		batch.setBatchOrderQty(totalOrders);
-		//this.queryManager.insert(batch);
+		// 5. Wave 소속 주문의 주문 수, 상품 수, PCS 수를 모두 계산
+		String sql = "select count(distinct(sku_cd)) as sku_qty, sum(order_qty) as total_pcs from orders where domain_id = :domainId and batch_id = :batchId";
+		Map<String, Object> condition = ValueUtil.newMap("domainId,batchId", wave.getDomainId(), wave.getId());
+		Wave orderQty = this.queryManager.selectBySql(sql, condition, Wave.class);
+		wave.setOrderQty(totalOrders);
+		wave.setSkuQty(orderQty.getSkuQty());
+		wave.setTotalPcs(orderQty.getTotalPcs());
+		this.queryManager.insert(wave);
 		
-		// 6. 배치 리턴 
-		return batch;
+		// 6. 커스텀 서비스 호출
+		Map<String, Object> parameters = ValueUtil.newMap("event,wave", event, wave);
+		this.customService.doCustomService(event.getDomainId(), DIY_HANDLE_BATCHES_BY_NOSNOS, parameters);
+		
+		// 7. 배치 리턴 
+		return wave;
 	}
 	
 	/**
 	 * 새로운 배치 생성
 	 * 
 	 * @param event
-	 * @param batchSeq
+	 * @param waveSeq
 	 * @return
 	 */
-	private JobBatch createNewBatch(BatchReceiveEvent event, int batchSeq) {
+	private Wave createNewWave(BatchReceiveEvent event, int waveSeq) {
 		String jobDate = event.getJobDate();
-		String batchId = event.getJobDate().replace(LogisConstants.DASH, LogisConstants.EMPTY_STRING) + LogisConstants.DASH + batchSeq;
 		String areaCd = event.getAreaCd();
-		String stageCd = event.getStageCd();
-		String comCd = event.getComCd();
+		String waveId = areaCd + jobDate.replace(LogisConstants.DASH, LogisConstants.EMPTY_STRING) + LogisConstants.DASH + waveSeq;
 		
-		JobBatch batch = new JobBatch();
-		batch.setDomainId(event.getDomainId());
-		batch.setId(batchId);
-		batch.setAreaCd(areaCd);
-		batch.setStageCd(stageCd);
-		batch.setComCd(comCd);
-		batch.setJobDate(jobDate);
-		batch.setJobSeq(ValueUtil.toString(batchSeq));
-		batch.setResultBoxQty(0);
-		batch.setResultOrderQty(0);
-		batch.setResultPcs(0);
-		batch.setResultSkuQty(0);
-		return batch;
+		Wave wave = new Wave();
+		wave.setDomainId(event.getDomainId());
+		wave.setId(waveId);
+		wave.setWaveNo(waveId);
+		wave.setComCd(event.getComCd());
+		wave.setJobDate(jobDate);
+		wave.setJobSeq(ValueUtil.toString(waveSeq));
+		wave.setStatus(JobBatch.STATUS_WAIT);
+		return wave;
 	}
 	
 	/**
-	 * 작업 배치로 주문 생성
+	 * Wave로 주문 생성
 	 * 
-	 * @param batch
+	 * @param event
+	 * @param wave
 	 * @param result
 	 */
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	private void createOrdersByBatch(JobBatch batch, Map<String, Object> result) {
+	private void createOrdersByWave(BatchReceiveEvent event, Wave wave, Map<String, Object> result) {
 		Map<String, Object> response = (Map<String, Object>)result.get("response");
+		Page<Map> ordersPage = this.parseDataPage(response, "release_info", "current_page", "total_page", "release_cnt");
+		
 		// 응답 데이터 중에 총 페이지, 현재 페이지, 주문 전체 정보 추출
-		int currentPage = ValueUtil.toInteger(response.get("current_page"));
-		int totalPage = ValueUtil.toInteger(response.get("total_page"));
-		List<Map> orderList = (List<Map>)response.get("release_info");
+		int currentPage = ordersPage.getIndex();
+		int totalPage = ordersPage.getSize();
+		List<Map> orderList = ordersPage.getList();
 		
 		if(ValueUtil.isNotEmpty(orderList)) {
 			// 주문 매핑 처리 - 커스텀 서비스 호출
-			Map<String, Object> parameters = ValueUtil.newMap("batch,orders", batch, orderList);
-			this.customService.doCustomService(batch.getDomainId(), DIY_HANDLE_ORDERS_BY_NOSNOS, parameters);
+			Map<String, Object> parameters = ValueUtil.newMap("wave,orders", wave, orderList);
+			this.customService.doCustomService(wave.getDomainId(), DIY_HANDLE_ORDERS_BY_NOSNOS, parameters);
 			
 			// 현재 페이지가 마지막 페이지가 아니면 ...
 			if(totalPage > currentPage) {
-				result = this.receiveOrdersByJobSeq(batch.getJobDate(), ValueUtil.toInteger(batch.getJobSeq()), currentPage + 1);
-				this.createOrdersByBatch(batch, result);
+				result = this.receiveOrdersByJobSeq(wave.getJobDate(), ValueUtil.toInteger(wave.getJobSeq()), currentPage + 1);
+				this.createOrdersByWave(event, wave, result);
 			}
 		}
 	}
 	
+	/**
+	 * NosNos API 조회 결과 파싱하기
+	 * 
+	 * @param result
+	 * @param responseField
+	 * @return
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private Map<String, ?> parseApiResult(Map<String, Object> result, String responseField) {
+		// 1. status 체크
+		if(result == null || !ValueUtil.toBoolean(result.get("status"))) {
+			throw ThrowUtil.newValidationErrorWithNoLog("수신 오류가 발생했습니다.");
+		}
+
+		// 2. response 객체 리턴
+		return (Map)result.get(ValueUtil.isEmpty(responseField) ? "response" : responseField);
+	}
+	
+	/**
+	 * 응답 데이터를 파싱하여 리스트로 리턴
+	 * 
+	 * @param response
+	 * @param listField
+	 * @param emptyErrorMsg
+	 * @return
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private List<?> parseDateList(Map<String, Object> response, String listField, String emptyErrorMsg) {
+		List<Map> dataList = (List<Map>)response.get(listField);
+		
+		if(ValueUtil.isEmpty(dataList)) {
+			throw ThrowUtil.newValidationErrorWithNoLog(emptyErrorMsg);
+		}
+		
+		return dataList;
+	}
+	
+	/**
+	 * 응답 데이터에서 총 레코드 수, 현재 페이지 수, 총 페이지 수, 데이터 리스트를 추출
+	 * 
+	 * @param response
+	 * @param listField
+	 * @param curPageField
+	 * @param totalPageField
+	 * @param totalCountField
+	 * @return
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private Page<Map> parseDataPage(Map<String, Object> response, String listField, String curPageField, String totalPageField, String totalCountField) {
+		// 응답 데이터 중에 총 페이지, 현재 페이지, 주문 전체 정보 추출
+		int currentPage = ValueUtil.toInteger(response.get(curPageField));
+		int totalPage = ValueUtil.toInteger(response.get(totalPageField));
+		int totalCount = ValueUtil.toInteger(response.get(totalCountField));
+		List<Map> orderList = (List<Map>)response.get(listField);
+		
+		Page<Map> page = new Page<Map>();
+		page.setSize(totalPage);
+		page.setIndex(currentPage);
+		page.setTotalSize(totalCount);
+		page.setList(orderList);
+		return page;
+	}
+	
+	/*************************************************************************************************/
+	/*												인 증 관 련 										 */
+	/*************************************************************************************************/
+	 
 	/**
 	 * 인증 헤더 구성
 	 * 
